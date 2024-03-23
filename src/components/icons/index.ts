1b69f0141967b51eda0fb426fc0bf896b2d705d7
Manjot Singh Gill <manjotg6@gmail.com>
2024-03-23 23:32:14 +0530

    setup vite react

--- src/components/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Github } from './github';
-export { default as LinkedIn } from './linkedin';
-export { default as Upwork } from './upwork';
-export { default as Hyphen } from './hyphen';
